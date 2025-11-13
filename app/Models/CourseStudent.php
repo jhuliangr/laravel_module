53365cf4b59c8f17bce1d9228a6b33008271196c
jhuliangr <jhpiano9731@gmail.com>
2025-11-13 17:54:37 +0100

    ADD course-student migration and model

---
+++ app/Models/CourseStudent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CourseStudent extends Model
+{
+    public function userData()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function courseData()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function homeworks()
+    {
+        return $this->hasMany(Homework::class);
+    }
+}
