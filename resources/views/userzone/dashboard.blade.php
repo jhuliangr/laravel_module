a8907ab7b3a94989f910816b667ccc739c7ef63b
jhuliangr <jhpiano9731@gmail.com>
2025-11-17 19:23:52 +0100

    ADD support for enrolling students in dashboard component

--- resources/views/userzone/dashboard.blade.php
+++ resources/views/userzone/dashboard.blade.php
@@ -1,25 +1,31 @@
 <x-site-layout>
-    <x-slot name="header">
-        <div class="flex items-center justify-between">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-            @if ($teacher)
-                You're logged in as a teacher
-            @endif
-        </div>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @if ($teacher)
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center px-3">
-                    Hello teacher!
-                    <a class="py-3 px-5 bg-teal-400 hover:bg-teal-300 rounded-xl cursor-pointer" href="{{ route('course.index') }}">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center px-3 py-2">
+                Hello
+                @if ($teacher)
+                    teacher!
+                @else
+                    student!
+                @endif
+
+                <a class="py-3 px-5 bg-teal-400 hover:bg-teal-300 rounded-xl cursor-pointer"
+                    href="{{ route('course.pick') }}">
+                    Select courses
+                </a>
+                @if ($teacher)
+                    <a class="py-3 px-5 bg-teal-400 hover:bg-teal-300 rounded-xl cursor-pointer"
+                        href="{{ route('course.index') }}">
                         Admin your courses
                     </a>
-                </div>
-            @endif
+                @endif
+            </div>
+            <div class="mt-10">
+                <a class="py-3 px-5 rounded-xl bg-teal-300" href="{{ route('courses.my_courses') }}">
+                    Go to my courses
+                </a>
+            </div>
+
         </div>
     </div>
 </x-site-layout>
