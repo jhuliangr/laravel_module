7afb42ad346763a7e54ce3438860f4de243f2432
jhuliangr <jhpiano9731@gmail.com>
2025-11-18 15:37:25 +0100

    FIX restyled dashboard

--- resources/views/userzone/dashboard.blade.php
+++ resources/views/userzone/dashboard.blade.php
@@ -1,31 +1,34 @@
 <x-site-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center px-3 py-2">
-                Hello
-                @if ($teacher)
-                    teacher!
-                @else
-                    student!
-                @endif
+    <x-app-layout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex justify-between items-center px-3 py-2">
+                    Hello
+                    @if ($teacher)
+                        teacher!
+                    @else
+                        student!
+                    @endif
 
-                <a class="py-3 px-5 bg-teal-400 hover:bg-teal-300 rounded-xl cursor-pointer"
-                    href="{{ route('course.pick') }}">
-                    Select courses
-                </a>
-                @if ($teacher)
                     <a class="py-3 px-5 bg-teal-400 hover:bg-teal-300 rounded-xl cursor-pointer"
-                        href="{{ route('course.index') }}">
-                        Admin your courses
+                        href="{{ route('course.pick') }}">
+                        Select courses
                     </a>
-                @endif
-            </div>
-            <div class="mt-10">
-                <a class="py-3 px-5 rounded-xl bg-teal-300" href="{{ route('courses.my_courses') }}">
-                    Go to my courses
-                </a>
-            </div>
+                    @if ($teacher)
+                        <a class="py-3 px-5 bg-teal-400 hover:bg-teal-300 rounded-xl cursor-pointer"
+                            href="{{ route('course.index') }}">
+                            Admin your courses
+                        </a>
+                    @endif
+                </div>
+                <div class="mt-10">
+                    <a class="py-3 px-5 rounded-xl bg-teal-300" href="{{ route('courses.my_courses') }}">
+                        Go to my courses
+                    </a>
+                </div>
 
+            </div>
         </div>
-    </div>
+    </x-app-layout>
 </x-site-layout>
