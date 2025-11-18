54b11f882c1f831ed88b9ece431cf9183fa8b447
jhuliangr <jhpiano9731@gmail.com>
2025-11-18 19:36:46 +0100

    ADD course-row to course.pick

--- resources/views/course/pick.blade.php
+++ resources/views/course/pick.blade.php
@@ -10,8 +10,7 @@
         </div>
     @else
         <div class="flex flex-col p-3 bg-gray-100 mt-5 max-h-[50vh] overflow-y-auto rounded-xl">
-            <div
-                class="font-bold grid grid-cols-2 bg-teal-300 my-1 px-5 py-1 rounded-xl">
+            <div class="font-bold grid grid-cols-2 bg-teal-300 my-1 px-5 py-1 rounded-xl">
                 <div>
                     Course name
                 </div>
@@ -20,15 +19,7 @@
                 </div>
             </div>
             @foreach ($courses as $course)
-                <a href="/course/{{ $course->id }}"
-                    class="grid grid-cols-2 bg-teal-200 my-1 px-5 py-1 rounded-xl cursor-pointer hover:bg-teal-100 transition-colors duration-300 ease-in">
-                    <div>
-                        {{ $course->module_name }}
-                    </div>
-                    <div>
-                        {{ $course->start_date }}
-                    </div>
-                </a>
+                <x-course-row :course="$course" />
             @endforeach
         </div>
     @endif
