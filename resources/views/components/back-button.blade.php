9a108a0e5f708d0e2210c6e1900ac0f45f9d78bc
jhuliangr <jhpiano9731@gmail.com>
2025-11-19 13:32:53 +0100

    FIX restyled back button

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ url()->previous() }}" class="bg-teal-300 p-2 rounded-xl">
+<a href="{{ url()->previous() }}" class="bg-teal-300 p-2 rounded-xl hover:bg-teal-400 transition-colors duration-300 px-5 shadow-md hover:shadow-sm">
     Back
 </a>
