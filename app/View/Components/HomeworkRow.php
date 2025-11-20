0e247ead4efae4bd44b86b1bd2c1bf0d570afaab
jhuliangr <jhpiano9731@gmail.com>
2025-11-20 12:58:37 +0100

    ADD homework row component

---
+++ app/View/Components/HomeworkRow.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Homework;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class HomeworkRow extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public Homework $hw;
+    public function __construct(Homework $hw)
+    {
+        $this->hw = $hw;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.homework-row');
+    }
+}
---
+++ resources/views/components/homework-row.blade.php
@@ -0,0 +1,5 @@
+<div class="my-1 py-2 px-5 bg-gradient-to-r from-teal-200 to-teal-600 hover:from-teal-500 hover:to-teal-100 rounded-xl shadow-md hover:shadow-sm">
+    <a href="/homework/{{ $hw->id }}">
+        <h2 class="">{{ $hw->title }}</h2>
+    </a>
+</div>
