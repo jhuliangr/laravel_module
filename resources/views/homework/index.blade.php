500eafee4bf8eda991d69d459b9420815b6ac1a5
jhuliangr <jhpiano9731@gmail.com>
2025-11-22 13:07:07 +0100

    ADD basic views for homeworks

--- resources/views/homework/index.blade.php
+++ resources/views/homework/index.blade.php
@@ -1,3 +1,13 @@
 <x-site-layout>
-  asd
+    @if ($homeworks->isEmpty())
+        <h3>No homeworks found</h3>
+    @else
+        <div class="flex items-center justify-between mb-5">
+            <x-back-button />
+            @include('components.homework-form', ['id' => $courseId])
+        </div>
+        @foreach ($homeworks as $hw)
+            <x-homework-row :hw="$hw" />
+        @endforeach
+    @endif
 </x-site-layout>
--- resources/views/homework/show.blade.php
+++ resources/views/homework/show.blade.php
@@ -1,1 +1,14 @@
-testing {{$id}}
+<x-site-layout title="Homework">
+  <div class="flex items-center justify-between mb-5 px-5">
+    <x-back-button/>
+  </div>
+    <div class="text-center">
+        <p class="text-xl font-bold p-5">
+            {{ $homework->title }}
+        </p>
+        <textarea class="rounded-lg w-3/4 h-96 p-4 border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none overflow-y-auto" readonly>
+          {{ $homework->body }}
+        </textarea>
+
+    </div>
+</x-site-layout>
