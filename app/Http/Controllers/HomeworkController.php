6ca48fb3207e7ad1b8325b1d87832c08a1ca91dc
jhuliangr <jhpiano9731@gmail.com>
2025-11-22 13:06:36 +0100

    ADD homework controller

--- app/Http/Controllers/HomeworkController.php
+++ app/Http/Controllers/HomeworkController.php
@@ -2,21 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CourseStudent;
 use App\Models\Homework;
+use Illuminate\Http\Request;
 
 class HomeWorkController extends Controller
 {
-    public function index()
+    public function index(Request $request, string $courseId)
     {
-        $homeworks = Homework::all();
+        $user = auth()->user();
+        $courseStudent = CourseStudent::where('user_id', $user->id)
+            ->where('course_id', $courseId)
+            ->first();
 
-        return view('homework.index', compact('homeworks'));
+        if (!$courseStudent) {
+            return collect();
+        }
+        $homeworks = Homework::where('course_student_id', $courseStudent->id)->get();
+
+        return view('homework.index', compact('homeworks', 'courseId'));
     }
 
-    public function show($id)
+    public function show(string $id)
     {
         $homework = Homework::find($id);
 
         return view('homework.show', compact('homework'));
+    }
+
+    public function store(Request $request, string $id)
+    {
+        $user = auth()->user();
+        $validated = $request->validate([
+            'title' => ['required', 'string', 'min:5', 'max:255'],
+            'body' => ['required', 'string', 'min:5', 'max:3000'],
+        ]);
+
+        $courseStudentId = $user->courseStudent()->where('course_id', $id)->first()->id;
+
+        Homework::create([
+            'title' => $validated['title'],
+            'body' => $validated['body'],
+            'course_student_id' => $courseStudentId
+        ]);
+
+        return redirect(route('homework.index', $id));
     }
 }
